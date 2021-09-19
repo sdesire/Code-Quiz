492afe6e6390ad404ac84e771f51f6da44f9886c
sdesire <sdesire@ltu.edu>
2021-09-19 12:19:46 -0400

    code quiz

---
+++ questions.js
@@ -0,0 +1,111 @@
+/**set the time to zero*/
+var timer = 76;
+var timeCount;
+/**this is the timer funtion which will start counting as soon as the quiz starts*/
+function setupTimer() {
+  timeCount = setInterval(function () {
+    timer--;
+    var timeReset = (timeElement.textContent = "Time:" + " " + timer);
+    timer = timer;
+    if (timer <= 0) {
+      clearInterval(timeCount);
+
+      timeElement.textContent = timeReset;
+    }
+  }, 1000);
+}
+
+/**  Here is the event listener to start the timer and hide the quiz button*/
+document.addEventListener("click", function (event) {
+  if (event.target === btnElement) {
+    wrapperElement.style.display = "none";
+    setupTimer();
+    displayQuestions();
+  }
+});
+
+/**declare the index variable for the onclickHandler function**/
+var i = 0;
+
+/**Add a function to compare the answers and
+ * display each questions as the buttons are clicked.*/
+function onclickHandler(event) {
+  if (timer <= 0) {
+    clearInterval(timeCount);
+    divContEL.style.display = "none";
+    displayResult();
+  }
+  var answerText = event.target.textContent;
+  if (answerText === questions[i].answer) {
+    timer = timer;
+    responsDiv.setAttribute("style", "color: green");
+    responsDiv.textContent = "Correct";
+  } else {
+    responsDiv.setAttribute("style", "color: red");
+    responsDiv.textContent = "Wrong";
+    timer = timer - 15;
+  }
+
+  if (i < questions.length - 1) {
+    i++;
+
+    setTimeout(function () {
+      displayQuestions();
+      responsDiv.textContent = "";
+    }, 1000);
+  } else {
+    setTimeout(function () {
+      responsDiv.textContent = "";
+      displayResult();
+      clearInterval(timeCount);
+    }, 500);
+
+    divContEL.innerHTML = "";
+  }
+
+  /**Function to display users final score */
+  function displayResult() {
+    finishDiv.style.visibility = "visible";
+    timeElement.textContent = "Time:" + " " + timer;
+    var HighScores = timer;
+    localStorage.getItem(HighScores);
+    finalScore.textContent = "Your finally score is: " + HighScores;
+    localStorage.setItem("HighScores", HighScores);
+  }
+}
+/**function to show the last page  */
+function renderLastItem() {
+  var yourScore = localStorage.getItem("HighScores");
+  var yourInitial = localStorage.getItem("Initial");
+  if (yourScore && yourInitial === "") {
+    return;
+  }
+  finishDiv.textContent = "";
+  var finaPageEl = document.querySelector(".final-page");
+  finaPageEl.style.visibility = "visible";
+  var initialAndScore = document.querySelector("#staticEmail");
+  initialAndScore.value = yourInitial + ":" + " " + yourScore;
+}
+
+//** This event listner submit the initial and final score to the local storage */
+document.addEventListener("submit", function (event) {
+  event.preventDefault();
+  var initialInput = document.querySelector("#inputInitial").value;
+  if (initialInput === "") {
+    errMsg.setAttribute("style", "color: red");
+    errMsg.textContent = "Initial input field cannot be empty";
+  } else {
+    errMsg.textContent = "";
+    localStorage.getItem(initialInput);
+    localStorage.setItem("Initial", initialInput);
+    renderLastItem();
+  }
+});
+/**This function will refresh the page and send user back to begining page when go back button is clicked */
+function init() {
+  location.reload();
+}
+/**This function will  clear initial and score displayed on the final page */
+function clearScore() {
+  initialAndScore.value = "";
+}
